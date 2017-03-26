15985c3bdd331b9c180776b01400e9b0e951490a
simply_me_q <simply.me.queeny@gmail.com>
2017-03-25 22:09:16 -0400

    used localstorage to store all data

--- js/dal.js
+++ js/dal.js
@@ -2,28 +2,24 @@
  * Created by quennie on 2017-03-24.
  */
 const DAL = {
-  initDB: function() {
-    try {
-      DB.createDatabase();
-      console.log(db);
-      if (db) {
-        DB.createTables();
-      }
-    } catch(e) {
-      console.error(e);
-    }
-  },
-  save: function(options, callback) {
-    db.transaction(function(tx) {
-      tx.executeSql('INSERT INTO iteminfo (' +
-        'name, address, city, phone, email, make, model, year' +
-        ') VALUES (?,?,?,?,?,?,?,?);', options, callback, errorHandler);
-    },  errorHandler, successfulTransaction);
+  save: function(item) {
+    localStorage.setItem('itemId', item.id);
+    localStorage.setItem(item.id, JSON.stringify(item));
   },
   selectAll: function(callback) {
-    db.transaction(function(tx) {
-      console.info('getting data ...');
-      tx.executeSql('SELECT * FROM iteminfo;', [], callback, errorHandler);
-    }, errorHandler, successfulTransaction);
+    var data = [];
+    for (i=0; i < localStorage.length; i++) {
+      var key = localStorage.key(i);
+      if (key && key.indexOf('itemId') < 0) {
+        data.push(JSON.parse(localStorage.getItem(key)));
+      }
+    }
+    callback( data );
+  },
+  print: function() {
+    for (i=0; i < localStorage.length; i++) {
+      var key = localStorage.key(i);
+      console.info(key, localStorage.getItem(key));
+    }
   }
 };
